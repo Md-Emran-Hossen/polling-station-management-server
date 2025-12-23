c7ab5ab6fd8d93ff7290b84ec39dc732e17651b2
Md-Emran-Hossen <emran.cse21@gmail.com>
2025-12-23 09:46:25 +0600

    Implement database connection

--- index.js
+++ index.js
@@ -12,8 +12,7 @@
  //DB User: polling_station_db_user
  //DB Pass: vT1b7agAK2ZylwM3
  
-
-const uri = "mongodb+srv://<polling_station_db_user>:<vT1b7agAK2ZylwM3>@cluster0.ikm2v.mongodb.net/?appName=Cluster0";
+const uri = "mongodb+srv://polling_station_db_user:VWBWydTDxADFrwFc@cluster0.ikm2v.mongodb.net/?appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,6 +28,63 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+
+    const districtCollection = client.db("pollingStation").collection("districts");
+    const upazilaCollection = client.db("pollingStation").collection("upazilas");
+    const pollingStationCollection = client.db("pollingStation").collection("pollingStations");
+    
+      // district route
+      app.post("/districts", async (req, res) => {
+        const districts = req.body;
+         console.log("District Info: ", districts);
+        const result = await districtCollection.insertOne(districts);
+     //   console.log("District Info: ", result);
+        res.send(result);
+      });
+
+      app.get("/districts", async (req, res) => {
+        const query = districtCollection.find();
+        const result = await query.toArray();
+        res.send(result);
+      });
+
+      app.delete("/district/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await districtCollection.deleteOne(query);
+        res.send(result);
+      });
+  
+      app.get("/district/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await districtCollection.findOne(query);
+        res.send(result);
+      });
+
+       app.put("/district/:id", async (req, res) => {
+        const dId = req.params.id;
+        console.log("Update Data Found",dId);
+        const district = req.body;
+        const filter = { _id: new ObjectId(dId) };
+        const option = { upsert: true };
+        
+        const updatedData = {
+          $set: {
+            districtName: district.districtName,
+            description: district.description,
+          },
+        };
+
+        const result = await districtCollection.updateOne(
+          filter,
+          updatedData,
+          option
+        );
+        res.send(result);
+      });
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
