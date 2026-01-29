1b06c6272dd647934e286f28978597ca46009305
Md-Emran-Hossen <emran.cse21@gmail.com>
2026-01-30 01:07:59 +0600

    Implement law enforcement module successfully

--- index.js
+++ index.js
@@ -6,8 +6,7 @@
 
  const app = express();
 
-
- // Handle CSP (Content Security Policy)
+// Handle CSP (Content Security Policy)
 app.use((req, res, next) => {
   res.setHeader("Content-Security-Policy", "default-src 'self'; connect-src 'self' http://localhost:5000;");
   next();
@@ -23,7 +22,7 @@ app.use((req, res, next) => {
  
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_SECRET}@cluster0.ikm2v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-       //      "mongodb+srv://polling_station_db_user:VWBWydTDxADFrwFc@cluster0.ikm2v.mongodb.net/?appName=Cluster0";
+// "mongodb+srv://polling_station_db_user:VWBWydTDxADFrwFc@cluster0.ikm2v.mongodb.net/?appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -46,6 +45,258 @@ async function run() {
     const pollingStationCollection = client.db("pollingStation").collection("pollingStations");
     const summaryCollection = client.db("pollingStation").collection("summaryInformations");
     const psCollection = client.db("pollingStation").collection("prisidingOfficers");
+
+    const armyCollection = client.db("pollingStation").collection("armys");
+    const bgbCollection = client.db("pollingStation").collection("bgbs");
+    const policeCollection = client.db("pollingStation").collection("polices");
+    const rabCollection = client.db("pollingStation").collection("rabs");
+    const magistrateCollection = client.db("pollingStation").collection("magistrates");
+
+   // magistrate route
+    app.post("/magistrates", async (req, res) => {
+        const magistrateInfo = req.body;
+        const result = await magistrateCollection.insertOne(magistrateInfo);
+        res.send(result);
+      });
+
+      app.get("/magistrates", async (req, res) => {
+        const query = magistrateCollection.find();
+        const result = await query.toArray();
+        res.send(result);
+      });
+
+      app.delete("/magistrate/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await magistrateCollection.deleteOne(query);
+        res.send(result);
+      });
+  
+      app.get("/magistrate/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await magistrateCollection.findOne(query);
+        res.send(result);
+      });
+
+       app.put("/magistrate/:id", async (req, res) => {
+        const mId = req.params.id;
+        const magistrateInfo = req.body;
+        const filter = { _id: new ObjectId(mId) };
+        const option = { upsert: true };
+        
+        const updatedData = {
+          $set: {
+            magistrateName: magistrateInfo.magistrateName,
+            designation: magistrateInfo.designation,
+            mobile: magistrateInfo.mobile,
+            pollingStations: magistrateInfo.pollingStations,
+          },
+        };
+
+        const result = await magistrateCollection.updateOne(
+          filter,
+          updatedData,
+          option
+        );
+        res.send(result);
+      });
+
+     // RAB route
+    app.post("/rabs", async (req, res) => {
+        const rabInfo = req.body;
+        const result = await rabCollection.insertOne(rabInfo);
+        res.send(result);
+      });
+
+      app.get("/rabs", async (req, res) => {
+        const query = rabCollection.find();
+        const result = await query.toArray();
+        res.send(result);
+      });
+
+      app.delete("/rab/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await rabCollection.deleteOne(query);
+        res.send(result);
+      });
+  
+      app.get("/rab/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await rabCollection.findOne(query);
+        res.send(result);
+      });
+
+       app.put("/rab/:id", async (req, res) => {
+        const rId = req.params.id;
+        const rabInfo = req.body;
+        const filter = { _id: new ObjectId(rId) };
+        const option = { upsert: true };
+        
+        const updatedData = {
+          $set: {
+            rabName: rabInfo.rabName,
+            designation: rabInfo.designation,
+            mobile: rabInfo.mobile,
+          },
+        };
+
+        const result = await rabCollection.updateOne(
+          filter,
+          updatedData,
+          option
+        );
+        res.send(result);
+      });
+
+    // Police route
+     app.post("/polices", async (req, res) => {
+        const policeInfo = req.body;
+        const result = await policeCollection.insertOne(policeInfo);
+        res.send(result);
+      });
+
+      app.get("/polices", async (req, res) => {
+        const query = policeCollection.find();
+        const result = await query.toArray();
+        res.send(result);
+      });
+
+      app.delete("/police/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await policeCollection.deleteOne(query);
+        res.send(result);
+      });
+  
+      app.get("/police/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await policeCollection.findOne(query);
+        res.send(result);
+      });
+
+       app.put("/police/:id", async (req, res) => {
+        const pId = req.params.id;
+        const policeInfo = req.body;
+        const filter = { _id: new ObjectId(pId) };
+        const option = { upsert: true };
+        
+        const updatedData = {
+          $set: {
+            policeName: policeInfo.policeName,
+            designation: policeInfo.designation,
+            mobile: policeInfo.mobile,
+          },
+        };
+
+        const result = await policeCollection.updateOne(
+          filter,
+          updatedData,
+          option
+        );
+        res.send(result);
+      });
+
+    // BGB route
+    app.post("/bgbs", async (req, res) => {
+        const bgbInfo = req.body;
+        const result = await bgbCollection.insertOne(bgbInfo);
+        res.send(result);
+      });
+
+      app.get("/bgbs", async (req, res) => {
+        const query = bgbCollection.find();
+        const result = await query.toArray();
+        res.send(result);
+      });
+
+      app.delete("/bgb/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await bgbCollection.deleteOne(query);
+        res.send(result);
+      });
+  
+      app.get("/bgb/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await bgbCollection.findOne(query);
+        res.send(result);
+      });
+
+       app.put("/bgb/:id", async (req, res) => {
+        const bId = req.params.id;
+        const bgbInfo = req.body;
+        const filter = { _id: new ObjectId(bId) };
+        const option = { upsert: true };
+        
+        const updatedData = {
+          $set: {
+            bgbName: bgbInfo.bgbName,
+            designation: bgbInfo.designation,
+            mobile: bgbInfo.mobile,
+          },
+        };
+
+        const result = await bgbCollection.updateOne(
+          filter,
+          updatedData,
+          option
+        );
+        res.send(result);
+      });
+
+    // Army route
+    app.post("/armys", async (req, res) => {
+        const armys = req.body;
+        const result = await armyCollection.insertOne(armys);
+        res.send(result);
+      });
+
+      app.get("/armys", async (req, res) => {
+        const query = armyCollection.find();
+        const result = await query.toArray();
+        res.send(result);
+      });
+
+      app.delete("/army/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await armyCollection.deleteOne(query);
+        res.send(result);
+      });
+  
+      app.get("/army/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await armyCollection.findOne(query);
+        res.send(result);
+      });
+
+       app.put("/army/:id", async (req, res) => {
+        const aId = req.params.id;
+        const armyInfo = req.body;
+        const filter = { _id: new ObjectId(aId) };
+        const option = { upsert: true };
+        
+        const updatedData = {
+          $set: {
+            armyName: armyInfo.armyName,
+            designation: armyInfo.designation,
+            mobile: armyInfo.mobile,
+          },
+        };
+
+        const result = await armyCollection.updateOne(
+          filter,
+          updatedData,
+          option
+        );
+        res.send(result);
+      });
 
       // district route
       app.post("/districts", async (req, res) => {
@@ -333,6 +584,8 @@ async function run() {
                   mapInfo: pollingStation.mapInfo,
                   prisidingOffcer: pollingStation.prisidingOffcer,
                   mobile: pollingStation.mobile,
+                  subInspector : pollingStation.subInspector,
+                  siMobile : pollingStation.siMobile,
           },
         };
 
